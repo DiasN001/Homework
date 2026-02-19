b4aa5597e9c168d2ba4015eaf2b2e989a3fa3a5a
DiasN001 <dias.niyazaly@jihc.edu.kz>
2026-02-19 15:14:30 +0500

    commit1

---
+++ js.js
@@ -0,0 +1,68 @@
+/*const students = ["Ali", "Dana", "Aruzhan", "Nursultan", "Madi", "Aigerim"];
+const original = [...students];
+
+azBtn.addEventListener("click", function() {
+  const sorted = [...students].sort();
+  render(sorted);
+});
+
+zaBtn.addEventListener("click", function() {
+  const sorted = [...students].sort().reverse();
+  render(sorted);
+});
+
+resetBtn.addEventListener("click", function() {
+  render(original);
+});*/
+
+
+/*if (courseValue !== "all") {
+    result = result.filter(function(s) {
+      return s.course === Number(courseValue);
+    });
+  }
+
+  if (sortValue === "az") {
+    result = [...result].sort(function(a, b) {
+      return a.name.localeCompare(b.name);
+    });
+  }
+
+  if (sortValue === "za") {
+    result = [...result].sort(function(a, b) {
+      return b.name.localeCompare(a.name);
+    });
+  }
+
+  render(result);
+if (courseValue !== "all") {
+    result = result.filter(function(s) {
+      return s.course === Number(courseValue);
+    });
+  }
+
+  if (sortValue === "az") {
+    result = [...result].sort(function(a, b) {
+      return a.name.localeCompare(b.name);
+    });
+  }
+
+  if (sortValue === "za") {
+    result = [...result].sort(function(a, b) {
+      return b.name.localeCompare(a.name);
+    });
+  }
+
+  render(result);
+  render(students);
+
+searchInput.addEventListener("input", applyAll);
+courseSelect.addEventListener("change", applyAll);
+sortSelect.addEventListener("change", applyAll);
+
+resetBtn.addEventListener("click", function() {
+  searchInput.value = "";
+  courseSelect.value = "all";
+  sortSelect.value = "none";
+  render(students);
+});*/
